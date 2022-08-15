67d3d6d7fa990767c1fbcf8537577732a5750ad0
rohitha <rohitha@LAPTOP-K6QNP36V>
2022-08-15 18:10:36 +0530

    First Commit.....

---
+++ src/Testcases/Test1.java
@@ -0,0 +1,11 @@
+package Testcases;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		String s="Rohitha";
+		System.out.println("Hello " +s+ " Welcome to Automation testing");
+	}
+
+}
